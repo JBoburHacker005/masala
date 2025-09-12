232a73960daa75020a81fd802a13688ad330818e
꧁𓆩༺  ⒷⓄⒷⓊⓇ  ༻𓆪꧂ <jbobur005@gmail.com>
2025-09-12 06:26:14 -0700

    Add files via upload

---
+++ main.py
@@ -0,0 +1,51 @@
+# # Wallrus
+#
+# # 1-masala
+# ot = input("Ismingizni kiriting: ")
+# print(f"Salom {ism := ot} loyihamizga xush kelibsiz!")
+#
+# # 2-masala
+# yil = int(input("Yoshingizni kiriting: "))
+# print(f"Siz {yosh := yil} yoshda ekansiz!")
+#
+# # 3-masala
+# yosh = int(input("Yoshingizni kiriting: "))
+# yil = int(input("Hozirgi yilni kiriting: "))
+# sana = yil - yosh
+# print(f"Siz {tugilgan := sana} yilda tug'ilgan ekansiz!")
+#
+# # 4-masala
+# a = int(input("Qo'shiluvchini kiriting: "))
+# b = int(input("Qo'shiluvchini kiriting: "))
+# print(f"Yig'indi teng: {c := a+b}")
+#
+# # 5-masala
+# boyi = int(input("Bo'yini kiriting: "))
+# eni = int(input("Enini kiriting: "))
+# print(f"To'g'ri to'rtburchak yuzi teng: {yuzi := boyi * eni}")
+#
+# # 6-masala
+# pi = 3.14
+# r = int(input("Aylana radiusini kiriting: "))
+# print(f"Aylana yuzi teng: {yuzi := 2 ** pi * r}")
+#
+# # 7-masala
+# ism = input("Ismingizni kiriting: ")
+# yosh = input("Yoshizni kiriting: ")
+# print(f"Ma'lumot: {malumot := ism, yosh}")
+#
+# # 8-masala
+# asos = int(input("Asosni kiritng: "))
+# balandlik = int(input("Balandlikni kiritng: "))
+# print(f"Uchburchak yuzi teng: {yuzi := (asos * balandlik) / 2}")
+#
+# # 9-masala
+# son = int(input("Sonni kiriting: "))
+# print(f"Sonning kvadrati teng: {kvadrat := son ** 2}")
+#
+# # 10-masala
+# a = int(input("Sonni kiriting: "))
+# b = int(input("Sonni kiriting: "))
+# c = int(input("Sonni kiriting: "))
+# d = [a,b,c]
+# print(f"Raqamlarning o'rtacha qiymati teng: {ortacha := sum(d) / len(d)}")
