1039491416b848622847f34ee177cf72025b05dd
꧁𓆩༺  ⒷⓄⒷⓊⓇ  ༻𓆪꧂ <jbobur005@gmail.com>
2025-10-03 18:11:12 +0500

    new

---
+++ masala.py
@@ -0,0 +1,96 @@
+# a = int(input())
+# b = int(input())
+# print(a+b)
+
+# a = int(input())
+# b = int(input())
+# print(a*b)
+
+# a = int(input())
+# b = int(input())
+# c = int(input())
+# print((a+b+c) / 3)
+
+# a = int(input())
+# b = int(input())
+# print(2*(a+b))
+
+# a = int(input())
+# b = int(input())
+# print(a*60+m)
+
+# a = int(input())
+# print(a*12000)
+
+# a = int(input())
+# b = int(input())
+# print(abs(a - b))
+
+# a = int(input())
+# b = int(input())
+# print(a**b)
+
+# a = int(input())
+# b = int(input())
+# print(a//b)
+
+# a = int(input())
+# b = int(input())
+# a, b = b, a
+# print(a,b)
+
+
+
+
+
+# a = int(input())
+# b = int(input())
+# print(a/b)
+
+# a = int(input())
+# b = int(input())
+# print(a/b)
+
+# a = int(input())
+# b = int(input())
+# c = b//a
+# d = b%a
+# print(c,d)
+
+# a = float(input())
+# b = int(input())
+# c = a/100 *b
+# print(c)
+
+# a = int(input())
+# b = a - a*0.12
+# print(b)
+
+# a = int(input())
+# b = int(input())
+# c = a*0.001*b
+# d = a + c
+# print(d)
+
+# a = int(input())
+# b = int(input())
+# d = a*(1-b/100)
+# print(d)
+
+# a = float(input())
+# b = float(input())
+# c = float(input())
+# d = (a*b*c)/100
+# e = a + d
+# print(e)
+
+# a = int(input())
+# b = int(input())
+# c = a/4
+# d = c*b
+# print(d)
+
+# a = int(input())
+# b = a*0.1
+# c = a + b
+# print(c)
