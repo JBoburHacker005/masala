35d3b76bf1141996ddef15cb43f9691bd5f7b83f
꧁𓆩༺  ⒷⓄⒷⓊⓇ  ༻𓆪꧂ <jbobur005@gmail.com>
2025-10-06 18:49:19 +0500

    5ta

---
+++ 20ta.py
@@ -0,0 +1,41 @@
+# import math
+# a = input().lower()
+# b = 47
+# c = 22000
+# d = 1.5
+# if a == "dushanba" or a == "seshanba" or a == "chorshanba" or a == "payshanba"or a == "juma":
+#     print(b * c)
+# elif a == "shanba" or a == "yakshanba":
+#     print(math.ceil(b * c * 1.5))
+# else:
+#     print("Error")
+
+# a = 8200
+# b = 0.7
+# c = 8200 / 0.7
+# print(f"{c:.2f}")
+
+# a = 4
+# b = 60
+# c = 6
+# d = 6 * 60 * 4 / 100
+# print(d)
+
+# a = 450 * 3
+# b = 5 * 3
+# c = 250 * 3
+# print(a, b, c)
+
+# a = float(input())
+# b = float(input())
+# c = a / b ** 2
+# if 0 <= c <= 18.5:
+#     print("ozg'in")
+# elif 18.5 <= c <= 24.9:
+#     print("ideal")
+# elif 25 <= c <= 29.9:
+#     print("o'rtacha")
+# elif c <= 30:
+#     print("semizlik")
+# else:
+#     print("Error")
