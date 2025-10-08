ff8937251c5d442716c401d8ab1c290250dd4f4a
꧁𓆩༺  ⒷⓄⒷⓊⓇ  ༻𓆪꧂ <jbobur005@gmail.com>
2025-10-08 18:36:11 +0500

    Update 20ta.py

--- 20ta.py
+++ 20ta.py
@@ -1,3 +1,4 @@
+# # 1
 # import math
 # a = input().lower()
 # b = 47
@@ -10,22 +11,26 @@
 # else:
 #     print("Error")
 
+# # 2
 # a = 8200
 # b = 0.7
 # c = 8200 / 0.7
 # print(f"{c:.2f}")
 
+# # 3
 # a = 4
 # b = 60
 # c = 6
-# d = 6 * 60 * 4 / 100
+# d = 6 * 60 * 4 / 1000
 # print(d)
 
+# # 4
 # a = 450 * 3
 # b = 5 * 3
 # c = 250 * 3
 # print(a, b, c)
 
+# # 5
 # a = float(input())
 # b = float(input())
 # c = a / b ** 2
@@ -39,3 +44,120 @@
 #     print("semizlik")
 # else:
 #     print("Error")
+
+# # 6
+# a = 1200
+# b = (((((((((1200 * 0.988)* 0.988)* 0.988)* 0.988)* 0.988)* 0.988)* 0.988)* 0.988)* 0.988)* 0.988
+# print(f"{b:.1f}")
+
+# # 7
+# import math
+# a = 480
+# b = 0.12 * 480
+# c = a / b
+# print(math.ceil(c))
+
+# # 8
+# a = 0
+# b = 90
+# c = 270
+# d = 360
+# c = 360 - 30 - 75
+# if c >= 360 :
+#     print(c - 360)
+# else:
+#     print("Janubiy G'arb")
+
+# # 9
+# import math
+# a = 20
+# b = 8
+# s = math.pi * a ** 2 / b
+# print(s)
+
+# # 10
+# a = 12000
+# b = 25000
+# c = 9000
+# d = a + b + c
+# if d > 40000:
+#     e = d * 0.9
+# else:
+#     e = d
+# print(e)
+
+# # 11
+# a = float(input())
+# b = a ** 3 * (1 + 0.008)
+# print(b)
+
+# # 12
+# a = 7.5
+# b = 3.75
+# c = a * 4 + b
+# print(c)
+
+# # 13
+# a = float(input())
+# if a <= 2:
+#     b = 10000
+# else:
+#     b = 10000 + (a - 2) * 4000
+# print(b)
+
+# # 14
+# a = 8.3
+# b = 780000
+# c = a * b
+# d = c * 0.02
+# e = c + d
+# print(e)
+
+# # 15
+# from math import sqrt
+# a = 550
+# b = 6371
+# c = sqrt(2 * a * b + a ** 2)
+# print(c)
+
+# # 16
+# a = 0.18
+# b = 5
+# c = 800
+# d = 12
+# e = c * d
+# f = e * a
+# g = (f * b) / 1000
+# print(g)
+
+# # 17
+# from math import sqrt
+# a = float(input())
+# b = float(input())
+# d = sqrt(a**2 + b**2)
+# print(d)
+
+# # 18
+# a = 10000000
+# b = 0.02
+# c = 12
+# d = a * (1 + b) ** c
+# print(d)
+
+# # 19
+# a = 0
+# b = 1000
+# c = 5
+# d = (b - a) / c
+# e = 12
+# f = 0.5 * d * e ** 2
+# print(d)
+# print(f)
+
+# # 20
+# a = 1200
+# b = 12560
+# c = a * b
+# d = c * 0.96
+# e = d * 0.88
+# print(e)
